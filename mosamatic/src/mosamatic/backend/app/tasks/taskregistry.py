7ae4fbe18d10accdba5a69fd181ad9c13aac9b39
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-03-13 13:44:44 +0100

    Cleaned up task registry

--- mosamatic/src/mosamatic/backend/app/tasks/taskregistry.py
+++ mosamatic/src/mosamatic/backend/app/tasks/taskregistry.py
@@ -4,92 +4,40 @@ from .mergedirectoriestask.mergedirectoriestask import MergeDirectoriesTask
 
 
 TASK_REGISTRY = {
+
     'ExampleTask': {
         'class': ExampleTask,
-        'title': 'Example Task with two inputs and one output',
-        'description': 'Some description',
+        'title': 'ExampleTask',
+        'description': 'Example Task with two inputs and one output',
         'inputs': [
-            {
-                'name': 'input1',
-                'label': 'Select input 1',
-            },
-            {
-                'name': 'input2',
-                'label': 'Select input 2',
-            },
+            {'name': 'input1', 'label': 'Select input 1'},
+            {'name': 'input2', 'label': 'Select input 2'},
         ],
-        'outputs': [
-            {
-                'name': 'output1',
-                'label': 'Enter name output 1 (optional)',
-            }
-        ],
+        'outputs': [{'name': 'output1', 'label': 'Enter name output 1 (optional)'}],
         'params': [
-            {
-                'name': 'nr_iters', 
-                'label': 'Number of iterations',
-                'type': 'int', 
-                'min': 0,
-                'max': 100,
-                'step': 1,
-                'value': 5,
-            },
-            {
-                'name': 'delay', 
-                'label': 'Delay (s)',
-                'type': 'int',
-                'min': 0,
-                'max': 10,
-                'step': 1,
-                'value': 1,
-            },
+            {'name': 'nr_iters', 'label': 'Number of iterations', 'type': 'int', 'min': 0, 'max': 100, 'step': 1, 'value': 5},
+            {'name': 'delay', 'label': 'Delay (s)', 'type': 'int', 'min': 0, 'max': 10, 'step': 1, 'value': 1},
         ],
     },
+    
     'CopyFilesTask': {
         'class': CopyFilesTask,
-        'title': 'Task that copies files from an input to an output directory',
-        'description': '',
-        'inputs': [
-            {
-                'name': 'input1',
-                'label': 'Select input 1',
-            },
-        ],
-        'outputs': [
-            {
-                'name': 'output1',
-                'label': 'Enter name output 1 (optional)',
-            }
-        ],
+        'title': 'CopyFilesTask',
+        'description': 'Task that copies files from an input to an output directory',
+        'inputs': [{'name': 'input1', 'label': 'Select input 1'}],
+        'outputs': [{'name': 'output1', 'label': 'Enter name output 1 (optional)'}],
         'params': [],
     },
+    
     'MergeDirectoriesTask': {
         'class': MergeDirectoriesTask,
-        'title': 'Task that copies files from two input directories to a single output directory',
-        'description': '',
+        'title': 'MergeDirectoriesTask',
+        'description': 'Task that copies files from two input directories to a single output directory',
         'inputs': [
-            {
-                'name': 'input1',
-                'label': 'Select input 1',
-            },
-            {
-                'name': 'input2',
-                'label': 'Select input 2',
-            },
+            {'name': 'input1', 'label': 'Select input 1'},
+            {'name': 'input2', 'label': 'Select input 2'},
         ],
-        'outputs': [
-            {
-                'name': 'output1',
-                'label': 'Enter name output 1 (optional)',
-            }
-        ],
-        'params': [
-            {
-                'name': 'keep_duplicates',
-                'label': 'Keep duplicates in merge (appends number if needed)',
-                'type': 'bool', 
-                'value': False,
-            }
-        ],
+        'outputs': [{'name': 'output1', 'label': 'Enter name output 1 (optional)'}],
+        'params': [{'name': 'keep_duplicates', 'label': 'Keep duplicates in merge (appends number if needed)', 'type': 'bool', 'value': False}],
     },
 }
