40a78b52be9c606f9fce2d1d6dba1da136ab3e1a
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-03-17 20:36:17 +0100

    Removed dummy tasks

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Mosamatic
 Web tool for body composition analysis from Maastricht University Medical Center (The Netherlands)
 
-# Development process
-- 
-
 # To-do
 - User manual
 - PyTorch model
--- mosamatic/src/mosamatic/backend/app/tasks/taskregistry.py
+++ mosamatic/src/mosamatic/backend/app/tasks/taskregistry.py
@@ -10,42 +10,6 @@ from .createpngsfromsegmentationstask.createpngsfromsegmentationstask import Cre
 
 TASK_REGISTRY = {
 
-    'ExampleTask': {
-        'class': ExampleTask,
-        'title': 'ExampleTask',
-        'description': 'Example Task with two inputs and one output',
-        'inputs': [
-            {'name': 'input1', 'label': 'Select input 1'},
-            {'name': 'input2', 'label': 'Select input 2'},
-        ],
-        'outputs': [{'name': 'output1', 'label': 'Enter name for output (optional)'}],
-        'params': [
-            {'name': 'nr_iters', 'label': 'Number of iterations', 'type': 'int', 'min': 0, 'max': 100, 'step': 1, 'value': 5},
-            {'name': 'delay', 'label': 'Delay (s)', 'type': 'int', 'min': 0, 'max': 10, 'step': 1, 'value': 1},
-        ],
-    },
-    
-    'CopyFilesTask': {
-        'class': CopyFilesTask,
-        'title': 'CopyFilesTask',
-        'description': 'Task that copies files from an input to an output directory',
-        'inputs': [{'name': 'input1', 'label': 'Select input 1'}],
-        'outputs': [{'name': 'output1', 'label': 'Enter name for output (optional)'}],
-        'params': [],
-    },
-    
-    'MergeDirectoriesTask': {
-        'class': MergeDirectoriesTask,
-        'title': 'MergeDirectoriesTask',
-        'description': 'Task that copies files from two input directories to a single output directory',
-        'inputs': [
-            {'name': 'input1', 'label': 'Select input 1'},
-            {'name': 'input2', 'label': 'Select input 2'},
-        ],
-        'outputs': [{'name': 'output1', 'label': 'Enter name for output (optional)'}],
-        'params': [{'name': 'keep_duplicates', 'label': 'Keep duplicates in merge (appends number if needed)', 'type': 'bool', 'value': False}],
-    },
-
     'DecompressDicomFilesTask': {
         'class': DecompressDicomFilesTask,
         'title': 'DecompressDicomFilesTask',
