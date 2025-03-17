d2a3c99543572081cbd1e55cab7ff4046b0bd998
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-03-17 07:46:30 +0100

    Savnig work

--- scripts/package.bat
+++ scripts/package.bat
@@ -6,8 +6,16 @@ copy /Y VERSION mosamatic\src\mosamatic\resources
 
 cd mosamatic
 
-briefcase update
-briefcase build --update-requirements
+rmdir /s /q "build"
+rmdir /s /q "dist"
+
+briefcase create
+briefcase build
+
+rmdir /s /q "build\mosamatic\windows\app\src\app_packages\tensorflow\include"
+del /q "build\mosamatic\windows\app\src\app\mosamatic\backend\db.sqlite3"
+del /q "build\mosamatic\windows\app\src\app\mosamatic\backend\app\migrations\*.py"
+
 briefcase package --adhoc-sign
 
 endlocal
