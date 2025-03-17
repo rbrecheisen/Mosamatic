c8ee361d921065b22265be29eb6dcdcdd52d858d
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-03-17 20:28:16 +0100

    Finished script for updating pyproject.toml version

--- scripts/updatetomlversion.py
+++ scripts/updatetomlversion.py
@@ -1,17 +1,36 @@
 import argparse
-import tomli
+import tomli # type: ignore
 import tomli_w
+
 
 VERSION = 'dev'
 with open('VERSION', 'r') as f:
     VERSION = f.readline().strip()
 
-with open("mosamatic/pyproject.toml", "rb") as f:
+
+with open('mosamatic/pyproject.toml', 'rb') as f:
     data = tomli.load(f)
 
-data["tool"]["poetry"]["version"] = VERSION
+data['tool']['briefcase']['version'] = VERSION
 
-with open("pyproject.toml", "wb") as f:
+with open('mosamatic/pyproject.toml', 'wb') as f:
     tomli_w.dump(data, f)
 
-print("Version updated! Run briefcase create to reload .toml")
+print('Version updated!')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('version', help='Version to use to update pyproject.toml')
+    args = parser.parse_args()
+
+    with open('mosamatic/pyproject.toml', 'rb') as f:
+        data = tomli.load(f)
+    data['tool']['briefcase']['version'] = args.version
+    with open('mosamatic/pyproject.toml', 'wb') as f:
+        tomli_w.dump(data, f)
+    print(f'Updated pyproject.toml version to {args.version}')
+
+
+if __name__ == '__main__':
+    main()
