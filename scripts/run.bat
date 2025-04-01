c49c789bd30c7bb3e43b81b87415204a845695f2
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-04-01 09:26:30 +0200

    Updated scripts

--- scripts/run.bat
+++ scripts/run.bat
@@ -3,34 +3,49 @@
 setlocal
 
 if "%*"=="" (
-    set START_DIR=%CD%
-    cd mosamatic
-    call briefcase dev
-    cd %START_DIR%
+    echo "Runs Mosamatic"
+    echo "Usage: run.bat [--dev|--build|--test|--docker]"
+    exit /b 1
 )
 
+set /p VERSION=<VERSION
+
 for %%A in (%*) do (
-    if /I "%%A"=="--docker" (
-        set /p VERSION=<VERSION
-        docker-compose up -d && docker-compose logs -f
+    if /I "%%A"=="--dev" (
+        set START_DIR=%CD%
+        cd mosamatic
+        call briefcase dev
+        cd %START_DIR%
+
+    ) else if /I "%%A"=="--build" (
+        set START_DIR=%CD%
+        cd mosamatic
+        del /q "build" "dist"
+        call briefcase create
+        call briefcase build
+        cd %START_DIR%
+
     ) else if /I "%%A"=="--test" (
         set START_DIR=%CD%
         cd mosamatic
         call briefcase dev --test
         cd %START_DIR%
-    ) else if /I "%%A"=="--build" (
-        set START_DIR=%CD%
-        cd mosamatic
-        call briefcase run
-        cd %START_DIR%
-    ) else if /I "%%A"=="--usage" (
-        echo "Usage: run.bat [--build|--test|--docker|--delete-db]"
-    ) else if /I "%%A"=="--delete-db" (
-        del /q mosamatic\src\mosamatic\backend\db.sqlite3
-        set START_DIR=%CD%
-        cd mosamatic
-        call briefcase dev
-        cd %START_DIR%
+
+    ) else if /I "%%A"=="--docker" (
+        set /p VERSION=<VERSION
+        call scripts\shutdown.bat
+        docker-compose build --no-cache
+        docker system prune -f
+        docker-compose up -d && docker-compose logs -f
+
+    ) else if /I "%%A"=="--production" (
+        set /p VERSION=<VERSION
+        call scripts\shutdown.bat --production
+        docker-compose -f docker-compose-prod.yml build --no-cache
+        docker system prune -f
+        docker-compose -f docker-compose-prod.yml up -d
+        docker-compose -f docker-compose-prod.yml logs -f
+
     )
 )
 
--- scripts/shutdown.bat
+++ scripts/shutdown.bat
@@ -4,7 +4,12 @@ setlocal
 
 set /p VERSION=<VERSION
 
-docker-compose down --volumes --remove-orphans
+if "%*"=="--production" (
+    docker-compose -f docker-compose-prod.yml down --volumes --remove-orphans
+) else (
+    docker-compose down --volumes --remove-orphans
+)
+
 docker container prune -f
 docker volume prune -f
 docker system prune -f
