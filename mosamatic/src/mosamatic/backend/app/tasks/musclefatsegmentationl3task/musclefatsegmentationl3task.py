7d61872df4586861214e3807d15a16e18bd714a4
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-04-07 07:56:03 +0200

    Uncommented tensorflow code and tasks

--- mosamatic/src/mosamatic/backend/app/tasks/musclefatsegmentationl3task/musclefatsegmentationl3task.py
+++ mosamatic/src/mosamatic/backend/app/tasks/musclefatsegmentationl3task/musclefatsegmentationl3task.py
@@ -1,74 +1,74 @@
-# import os
-# import numpy as np
+import os
+import numpy as np
 
-# from ..task import Task
-# from .tensorflowmodel import TensorFlowModel
-# # from .torchmodel import TorchModel
-# from ...utils import load_dicom, normalize_between, get_pixels_from_dicom_object, convert_labels_to_157
+from ..task import Task
+from .tensorflowmodel import TensorFlowModel
+# from .torchmodel import TorchModel
+from ...utils import load_dicom, normalize_between, get_pixels_from_dicom_object, convert_labels_to_157
 
 
-# class MuscleFatSegmentationL3Task(Task):
-#     def load_models_and_params(self, model_files, model_type, model_version):
-#         # if model_type == 'torch':
-#         #     torch_model = TorchModel()
-#         #     model, contour_model, params = torch_model.load(model_files, model_version)
-#         #     return model, contour_model, params
-#         if model_type == 'tensorflow':
-#             tensorflow_model = TensorFlowModel()
-#             model, contour_model, params = tensorflow_model.load(model_files, model_version)
-#             return model, contour_model, params
-#         else:
-#             raise RuntimeError(f'Unknown model type {model_type}')
+class MuscleFatSegmentationL3Task(Task):
+    def load_models_and_params(self, model_files, model_type, model_version):
+        # if model_type == 'torch':
+        #     torch_model = TorchModel()
+        #     model, contour_model, params = torch_model.load(model_files, model_version)
+        #     return model, contour_model, params
+        if model_type == 'tensorflow':
+            tensorflow_model = TensorFlowModel()
+            model, contour_model, params = tensorflow_model.load(model_files, model_version)
+            return model, contour_model, params
+        else:
+            raise RuntimeError(f'Unknown model type {model_type}')
 
-#     def predict_contour(self, contour_model, img, params, model_type):
-#         # if model_type == 'torch':
-#         #     torch_model = TorchModel()
-#         #     mask = torch_model.predict_contour(img, contour_model, params)
-#         #     return mask
-#         if model_type == 'tensorflow':
-#             tensorflow_model = TensorFlowModel()
-#             mask = tensorflow_model.predict_contour(img, contour_model, params)
-#             return mask
-#         else:
-#             raise RuntimeError(f'Unknown model type {model_type}')
+    def predict_contour(self, contour_model, img, params, model_type):
+        # if model_type == 'torch':
+        #     torch_model = TorchModel()
+        #     mask = torch_model.predict_contour(img, contour_model, params)
+        #     return mask
+        if model_type == 'tensorflow':
+            tensorflow_model = TensorFlowModel()
+            mask = tensorflow_model.predict_contour(img, contour_model, params)
+            return mask
+        else:
+            raise RuntimeError(f'Unknown model type {model_type}')
 
-#     def process_file(self, f_path, output_dir, model, contour_model, params, model_type):
-#         p = load_dicom(f_path)
-#         if p is None:
-#             self.log_warning(f'File {f_path} is not valid DICOM, skipping...')
-#             return
-#         img1 = get_pixels_from_dicom_object(p, normalize=True)        
-#         if contour_model:
-#             mask = self.predict_contour(contour_model, img1, params, model_type)
-#             img1 = normalize_between(img1, params['min_bound'], params['max_bound'])
-#             img1 = img1 * mask
-#         img1 = img1.astype(np.float32)
-#         # if model_type == 'torch':
-#         #     torch_model = TorchModel()
-#         #     pred_max = torch_model.predict(img1, model)
-#         if model_type == 'tensorflow':
-#             tensorflow_model = TensorFlowModel()
-#             pred_max = tensorflow_model.predict(img1, model)
-#         else:
-#             raise RuntimeError(f'Unknown model type {model_type}')
-#         pred_max = convert_labels_to_157(pred_max)
-#         segmentation_file_name = os.path.split(f_path)[1]
-#         segmentation_file_path = os.path.join(output_dir, f'{segmentation_file_name}.seg.npy')
-#         np.save(segmentation_file_path, pred_max)
-#         return segmentation_file_path
+    def process_file(self, f_path, output_dir, model, contour_model, params, model_type):
+        p = load_dicom(f_path)
+        if p is None:
+            self.log_warning(f'File {f_path} is not valid DICOM, skipping...')
+            return
+        img1 = get_pixels_from_dicom_object(p, normalize=True)        
+        if contour_model:
+            mask = self.predict_contour(contour_model, img1, params, model_type)
+            img1 = normalize_between(img1, params['min_bound'], params['max_bound'])
+            img1 = img1 * mask
+        img1 = img1.astype(np.float32)
+        # if model_type == 'torch':
+        #     torch_model = TorchModel()
+        #     pred_max = torch_model.predict(img1, model)
+        if model_type == 'tensorflow':
+            tensorflow_model = TensorFlowModel()
+            pred_max = tensorflow_model.predict(img1, model)
+        else:
+            raise RuntimeError(f'Unknown model type {model_type}')
+        pred_max = convert_labels_to_157(pred_max)
+        segmentation_file_name = os.path.split(f_path)[1]
+        segmentation_file_path = os.path.join(output_dir, f'{segmentation_file_name}.seg.npy')
+        np.save(segmentation_file_path, pred_max)
+        return segmentation_file_path
 
-#     def execute(self):
-#         input_files = self.input('images')
-#         model_files = self.input('model_files')
-#         model_type = self.param('model_type', 'tensorflow')
-#         model_version = self.param('model_version', 1.0)
-#         model, contour_model, params = self.load_models_and_params(model_files, model_type, model_version)
-#         if model is None or contour_model is None or params is None:
-#             raise RuntimeError('Model, contour model or parameters could not be loaded')
-#         nr_steps = len(input_files)
-#         for step in range(nr_steps):
-#             if self.is_canceled():
-#                 break
-#             source = input_files[step]
-#             self.process_file(source, self.output('segmentations'), model, contour_model, params, model_type)
-#             self.set_progress(step, nr_steps)
+    def execute(self):
+        input_files = self.input('images')
+        model_files = self.input('model_files')
+        model_type = self.param('model_type', 'tensorflow')
+        model_version = self.param('model_version', 1.0)
+        model, contour_model, params = self.load_models_and_params(model_files, model_type, model_version)
+        if model is None or contour_model is None or params is None:
+            raise RuntimeError('Model, contour model or parameters could not be loaded')
+        nr_steps = len(input_files)
+        for step in range(nr_steps):
+            if self.is_canceled():
+                break
+            source = input_files[step]
+            self.process_file(source, self.output('segmentations'), model, contour_model, params, model_type)
+            self.set_progress(step, nr_steps)
--- mosamatic/src/mosamatic/backend/app/tasks/musclefatsegmentationl3task/torchmodel.py
+++ mosamatic/src/mosamatic/backend/app/tasks/musclefatsegmentationl3task/torchmodel.py
@@ -1,52 +1,52 @@
-# import os
-# import json
-# import torch
-# import cv2
-# import numpy as np
+import os
+import json
+import torch
+import cv2
+import numpy as np
 
-# from models import UNet
-# from torch.nn import MaxPool2d, Sequential, Conv2d, PReLU, BatchNorm2d, Dropout, ConvTranspose2d
+from models import UNet
+from torch.nn import MaxPool2d, Sequential, Conv2d, PReLU, BatchNorm2d, Dropout, ConvTranspose2d
 
-# from ...utils import normalize_between
+from ...utils import normalize_between
 
 
-# class TorchModel:
-#     def load(self, model_files, model_version):
-#         model, contour_model, params = None, None, None
-#         for f_path in model_files:
-#             with torch.serialization.safe_globals([UNet, MaxPool2d, Sequential, Conv2d, PReLU, BatchNorm2d, Dropout, ConvTranspose2d]):
-#                 f_name = os.path.split(f_path)[1]
-#                 if f_name == f'model-{str(model_version)}.pth':
-#                     model = torch.load(f_path, map_location=torch.device('cpu'))
-#                     model.to('cpu')
-#                     model.eval()
-#                 elif f_name == f'contour_model-{str(model_version)}.pth':
-#                     contour_model = torch.load(f_path, map_location=torch.device('cpu'))
-#                     contour_model.to('cpu')
-#                     contour_model.eval()
-#                 elif f_name == f'params-{str(model_version)}.json':
-#                     with open(f_path, 'r') as obj:
-#                         params = json.load(obj)
-#                 else:
-#                     pass
-#         return model, contour_model, params
+class TorchModel:
+    def load(self, model_files, model_version):
+        model, contour_model, params = None, None, None
+        for f_path in model_files:
+            with torch.serialization.safe_globals([UNet, MaxPool2d, Sequential, Conv2d, PReLU, BatchNorm2d, Dropout, ConvTranspose2d]):
+                f_name = os.path.split(f_path)[1]
+                if f_name == f'model-{str(model_version)}.pth':
+                    model = torch.load(f_path, map_location=torch.device('cpu'))
+                    model.to('cpu')
+                    model.eval()
+                elif f_name == f'contour_model-{str(model_version)}.pth':
+                    contour_model = torch.load(f_path, map_location=torch.device('cpu'))
+                    contour_model.to('cpu')
+                    contour_model.eval()
+                elif f_name == f'params-{str(model_version)}.json':
+                    with open(f_path, 'r') as obj:
+                        params = json.load(obj)
+                else:
+                    pass
+        return model, contour_model, params
 
-#     def predict_contour(self, image, contour_model, params):
-#         ct = np.copy(image)
-#         ct = normalize_between(ct, params['min_bound_contour'], params['max_bound_contour'])
-#         target_shape = (512, 512)  
-#         ct_resized = cv2.resize(ct, target_shape, interpolation=cv2.INTER_LINEAR)
-#         ct_resized_tensor = torch.tensor(ct_resized, dtype=torch.float32).unsqueeze(0).unsqueeze(0).to('cpu')
-#         with torch.no_grad():
-#             pred = contour_model(ct_resized_tensor).cpu().numpy()
-#         pred_max = pred.argmax(axis=1)
-#         mask = np.uint8(pred_max)
-#         return mask
+    def predict_contour(self, image, contour_model, params):
+        ct = np.copy(image)
+        ct = normalize_between(ct, params['min_bound_contour'], params['max_bound_contour'])
+        target_shape = (512, 512)  
+        ct_resized = cv2.resize(ct, target_shape, interpolation=cv2.INTER_LINEAR)
+        ct_resized_tensor = torch.tensor(ct_resized, dtype=torch.float32).unsqueeze(0).unsqueeze(0).to('cpu')
+        with torch.no_grad():
+            pred = contour_model(ct_resized_tensor).cpu().numpy()
+        pred_max = pred.argmax(axis=1)
+        mask = np.uint8(pred_max)
+        return mask
 
-#     def predict(self, image, model):
-#         img1_tensor = torch.tensor(image, dtype=torch.float32).unsqueeze(0).to('cpu')
-#         with torch.no_grad():
-#             pred = model(img1_tensor).cpu().numpy()
-#         pred_squeeze = np.squeeze(pred)
-#         pred_max = pred_squeeze.argmax(axis=0)
-#         return pred_max
+    def predict(self, image, model):
+        img1_tensor = torch.tensor(image, dtype=torch.float32).unsqueeze(0).to('cpu')
+        with torch.no_grad():
+            pred = model(img1_tensor).cpu().numpy()
+        pred_squeeze = np.squeeze(pred)
+        pred_max = pred_squeeze.argmax(axis=0)
+        return pred_max
