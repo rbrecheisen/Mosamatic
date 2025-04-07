75c79be24205a60883e849a06cd8a2637a25665b
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-04-07 13:34:12 +0200

    Fix warning requests in requiresments.txt

--- scripts/deploy2dockerhub.bat
+++ scripts/deploy2dockerhub.bat
@@ -10,7 +10,6 @@ pause
 
 docker logout
 type C:\\Users\\r.brecheisen\\dockerhub.txt | docker login --username brecheisen --password-stdin
-@REM docker push brecheisen/mosamatic-nginx-intel:%VERSION%
 docker push brecheisen/mosamatic-web-intel:%VERSION%
 
 endlocal
