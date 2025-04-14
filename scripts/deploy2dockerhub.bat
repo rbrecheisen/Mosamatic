51c3e78130c66b37ec4ef5467881f1809413d207
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-04-14 14:20:31 +0200

    Deploy scripts first builds the image

--- scripts/deploy2dockerhub.bat
+++ scripts/deploy2dockerhub.bat
@@ -8,6 +8,8 @@ echo "Deploying Mosamatic version %VERSION% to DockerHub."
 echo "Is that correct?"
 pause
 
+call scripts\build-docker.bat
+
 docker logout
 type C:\\Users\\r.brecheisen\\dockerhub.txt | docker login --username brecheisen --password-stdin
 docker push brecheisen/mosamatic-web-intel:%VERSION%
