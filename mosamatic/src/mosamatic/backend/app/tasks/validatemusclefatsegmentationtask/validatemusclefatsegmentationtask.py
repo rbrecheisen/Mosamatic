9d5276e73ca0308eb79f581664db4083db901baf
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-06-23 14:22:15 +0200

    Working on validation task muscle/fat segmentation

--- mosamatic/src/mosamatic/backend/app/tasks/validatemusclefatsegmentationtask/validatemusclefatsegmentationtask.py
+++ mosamatic/src/mosamatic/backend/app/tasks/validatemusclefatsegmentationtask/validatemusclefatsegmentationtask.py
@@ -8,43 +8,22 @@ from ..task import Task
 LOG = LogManager()
 
 
-class ValidateSliceSelectionTask(Task):
-    def find_manual_file(self, auto_file, manually_selected):
-        p_auto = load_dicom(auto_file, stop_before_pixels=True)
-        for f in manually_selected:
-            p_manual = load_dicom(f, stop_before_pixels=True)
-            if p_manual.SeriesInstanceUID == p_auto.SeriesInstanceUID:
-                return f
+class ValidateMuscleFatSegmentationTask(Task):
+    def find_pred_mask_file(self, true_mask_file, pred_mask_files):
+        for pred_mask_file in pred_mask_files:
+            pred_mask_file_name = os.path.split(pred_mask_file)[1][:-8] # .seg.npy
+            true_mask_file_name = os.path.split(true_mask_file)[1][:-4] # .tag
+            if pred_mask_file_name == true_mask_file_name:
+                return pred_mask_file
         return None
     
-    def compare_z_positions(self, auto_file, manual_file):
-        p_auto = load_dicom(auto_file, stop_before_pixels=True)
-        z_auto = p_auto.ImagePositionPatient[2]
-        p_manual = load_dicom(manual_file, stop_before_pixels=True)
-        z_manual = p_manual.ImagePositionPatient[2]
-        z_diff = z_auto - z_manual
-        return z_auto, z_manual, z_diff
-
     def execute(self):
-        tensorflow_files = self.input('tensorflow')
-        torch_files = self.input('torch')
-        nr_steps = len(tensorflow_files)
-        data = {'file': [], 'z_auto': [], 'z_manual': [], 'z_diff': []}
+        true_mask_files = self.input('true_mask_files')
+        pred_mask_files = self.input('pred_mask_files')
+        nr_steps = len(true_mask_files)
         for step in range(nr_steps):
             if self.is_canceled():
                 break
-            # auto_file = tensorflow_files[step]
-            # manual_file = self.find_manual_file(auto_file, manually_selected)
-            # if manual_file is not None:
-            #     z_auto, z_manual, z_diff = self.compare_z_positions(auto_file, manual_file)
-            #     f_name = os.path.split(manual_file)[1]
-            #     data['file'].append(f_name)
-            #     data['z_auto'].append(z_auto)
-            #     data['z_manual'].append(z_manual)
-            #     data['z_diff'].append(z_diff)
-            # else:
-            #     LOG.warning(f'Could not find manually selected file for automatically selected file {auto_file}. Skipping...')
-            self.set_progress(step, nr_steps)
-        df = pd.DataFrame(data=data)
-        csv_file_path = os.path.join(self.output('comparison'), 'comparison.csv')
-        df.to_csv(csv_file_path, index=False, sep=';')
+            true_mask_file = true_mask_files[step]
+            pred_mask_file = self.find_pred_mask_file(true_mask_file, pred_mask_files)
+            print(pred_mask_file)
